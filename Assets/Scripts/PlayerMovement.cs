a88a4fa9ae413771e1c663097ed8c635395fea5b
Cedeno718 <cedeno718@morrisville.edu>
2024-11-26 18:13:39 -0500

    Set Up Input System

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-  Vector2 moveInput
+    Vector2 moveInput;
     void Start()
     {
         
@@ -15,8 +16,9 @@ public class PlayerMovement : MonoBehaviour
     {
         
     }
-void OnMove(InputValue value)
-{
-moveInput = value.Get<Vector2>();
-debug.Log(moveInput);
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
 }
