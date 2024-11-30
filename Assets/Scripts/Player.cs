5afb2e20c6bf04749aa4e3d9aeec8e12401c0f2d
Cedeno718 <cedeno718@morrisville.edu>
2024-11-30 17:24:27 -0500

    Player Mortality and other Changes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,43 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
-{
-    [SerializeField] float  moveSpeed = 5f;
-    Vector2 rawInput;
+// public class Player : MonoBehaviour
+// {
+//     [SerializeField] float  moveSpeed = 5f;
+//     Vector2 rawInput;
 
-    Vector2 minBounds;
-    Vector2 maxBounds;
+//     Vector2 minBounds;
+//     Vector2 maxBounds;
     
-    void Start() 
-    {
-      InitBounds();  
-    }
-    void Update()
-    {
-        Move();
-    }
-    void InitBounds()
-{
-    Camera mainCamera = Camera.main;
-    minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0,0));
-    maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1,1));
-}
+//     void Start() 
+//     {
+//       InitBounds();  
+//     }
+//     void Update()
+//     {
+//         Move();
+//     }
+//     void InitBounds()
+// {
+//     Camera mainCamera = Background.Tilemap;
+//     minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0,0));
+//     maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1,1));
+// }
 
-void Move()
-{
-    Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
-    Vector2 newPos = new Vector2();
+// void Move()
+// {
+//     Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+//     Vector2 newPos = new Vector2();
     
-    newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x, maxBounds.x);
-    newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y, maxBounds.y);
-    transform.position = newPos;
+//     newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x, maxBounds.x);
+//     newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y, maxBounds.y);
+//     transform.position = newPos;
 
 
-}
-void OnMove(InputValue value)
-{
-    rawInput = value.Get<Vector2>();
-    Debug.Log(rawInput);
-}
-}
+// }
+// void OnMove(InputValue value)
+// {
+//     rawInput = value.Get<Vector2>();
+//     Debug.Log(rawInput);
+// }
+// }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,43 +5,109 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float runSpeed = 10f;
-    Vector2 moveInput;
-    Rigidbody2D myRigidbody;
-    Animator myAnimator;
-    
+    [SerializeField] float runSpeed = 10f;    // Speed of horizontal movement
+    [SerializeField] float jumpSpeed = 5f;   // Force applied when jumping
+    Vector2 moveInput;                       // Stores player input
+    Rigidbody2D myRigidbody;                 // Reference to the Rigidbody2D
+    Animator myAnimator;                     // Reference to the Animator
+    CircleCollider2D myCircleCollider;       // Reference to the ground-check collider
+    BoxCollider2D myFeetCollider;
+
+    bool isAlive = true;
+
     void Start()
     {
-        myRigidbody =GetComponent<Rigidbody2D>();
-        myAnimator =GetComponent<Animator>();
-
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>(); 
+        myCircleCollider = GetComponent<CircleCollider2D>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
     }
 
-    
     void Update()
     {
+        if (!isAlive)
+        {
+            return;
+        }
         Run();
-        FlipSprite();
+        FlipSprite(); // Ensures the player faces the correct direction
+        Die();
+
+
+
+        // CheckIfJumping();
     }
+
     void OnMove(InputValue value)
     {
-        moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
-    }
-    void Run()
-    {
-        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
-        myRigidbody.velocity = playerVelocity;
-        
-        myAnimator.SetBool("isWalking", true);
-
-    }
-    void FlipSprite()
-    {
-        bool PlayerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
-        if (PlayerHasHorizontalSpeed)
+        if (!isAlive)
         {
-        transform.localScale = new Vector2 (Mathf.Sign(myRigidbody.velocity.x), 1f);
+            return;
+        }
+        // Capture horizontal movement input
+        moveInput = value.Get<Vector2>();
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (!isAlive)
+        {
+            return;
+        }
+        // Only allow jumping if the player is on the ground
+        if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+
+        if (value.isPressed)
+        {
+            // Apply vertical velocity for jumping
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpSpeed);
+
+            // Flip immediately if the player jumps with horizontal input
+            if (Mathf.Abs(moveInput.x) > Mathf.Epsilon)
+            {
+                transform.localScale = new Vector2(Mathf.Sign(moveInput.x), 1f);
+            }
         }
     }
+
+    void Run()
+    {
+        // Set player velocity based on horizontal input and run speed
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+
+        // Check if the player is moving horizontally
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isWalking", playerHasHorizontalSpeed);
+    }
+
+    void FlipSprite()
+    {
+        // Flip the sprite based on horizontal velocity
+        if (Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
+    }
+
+     void Die()
+    {
+        if (myCircleCollider.IsTouchingLayers(LayerMask.GetMask("Enemies")))
+        {
+            isAlive = false;
+        }
+    }
+
+
+    // void CheckIfJumping()
+    // {
+    //     // Check if the player is airborne by checking vertical velocity
+    //     bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody.velocity.y) > Mathf.Epsilon;
+
+    //     // Update the "isJumping" animator parameter
+    //     myAnimator.SetBool("isJumping", playerHasVerticalSpeed);
+    // }
 }
